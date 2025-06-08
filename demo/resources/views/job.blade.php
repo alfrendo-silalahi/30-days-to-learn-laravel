11a63e91abc44f2b904a337728ae65e08b1ffc83
alfrendo-silalahi <alfrendos72@gmail.com>
2025-06-08 18:49:29 +0700

    06 view data and route wildcards

---
+++ demo/resources/views/job.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Job
+    </x-slot:heading>
+    
+    <h1 class="font-bold text-lg">{{ $job['title'] }}</h1>
+    <p>This job pays {{ $job['salary'] }} per year</p>
+</x-layout>
